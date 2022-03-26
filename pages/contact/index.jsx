49151d611e4b057a1ab2fb2be98938a5e69b3474
Roman Valero <valeroman@gmail.com>
2022-03-26 08:36:55 -0500

    primer commit

---
+++ pages/contact/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Link from 'next/link';
+import { MainLayout } from '../../components/layouts/MainLayout';
+
+const ContactPage = () => {
+  return (
+    <MainLayout>
+      <h1>Contact Page</h1>
+      <h1 className={"title"}>
+      {/* Ir a <a href="/">Home</a> */}
+      Ir a <Link href="/">Home</Link>
+      </h1>
+
+      <p className={"description"}>
+      Get started by editing{' '}
+      <code className={"code"}>pages/contact.js</code>
+      </p>
+    </MainLayout>
+  )
+}
+
+export default ContactPage;
